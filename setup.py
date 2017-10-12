d1ecc996269a801c65d3b88791f7f5546c8af1b8
odan <odan3240@gmail.com>
2017-10-12 18:54:55 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='daria',
+    version='0.0.1',
+    description='pytorch trainer',
+    author='odanado',
+    author_email='odan3240@gmail.com',
+    url='https://github.com/odanado/daria',
+    license='MIT License',
+    packages=['daria'],
+    tests_require=['mock'],
+    test_suite='tests',
+)
